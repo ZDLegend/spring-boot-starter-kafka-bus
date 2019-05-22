c79f782fac7134f2c6cab2afd6013f3a94033784
张明浩 <463764941@qq.com>
2019-05-22 11:35:51 +0800

    继承接口后注入spring

--- spring-boot-starter-kafka-bus/src/main/java/com/zdl/spring/bus/kafka/KafkaOffsetInit.java
+++ spring-boot-starter-kafka-bus/src/main/java/com/zdl/spring/bus/kafka/KafkaOffsetInit.java
@@ -1,12 +1,12 @@
 package com.zdl.spring.bus.kafka;
 
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.kafka.support.TopicPartitionInitialOffset;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by ZDLegend on 2019/4/18 11:49
  */
-@Component
+@NoRepositoryBean
 public interface KafkaOffsetInit {
     TopicPartitionInitialOffset[] topicPartitionInitialOffset();
 }
